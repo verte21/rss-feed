95a0b8e0dde3166c655649b400e5148c432090c9
verte21 <[verte21rs@gmail.com]>
2022-05-02 00:23:45 +0200

    added jwt

--- server/app/middlewares/user-middleware.js
+++ server/app/middlewares/user-middleware.js
@@ -1,21 +1,17 @@
 import jwt from 'jsonwebtoken';
+import config from '../../config.js';
 
-export const verifyToken = (res, res, next) => {
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.split(' ')[0] == 'Bearer'
-  ) {
-    const token = req.headers.authorization.split(' ')[1];
-  }
+export const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
     return res.status(403).send('Token is required');
   }
-
   try {
-    req.user = jwt.verify(token, config.TOKEN_KEY);
+    const user = jwt.verify(token, config.access_token);
   } catch (err) {
     return res.status(401).send('Invalid Token');
   }
+
   return next();
 };
--- server/app/middlewares/view-variables-middleware.js
+++ server/app/middlewares/view-variables-middleware.js
@@ -1,7 +1,5 @@
-export function(req, res, next) {
-    res.locals.url = req.url;
-    res.locals.errors = null;
-    res.locals.query = req.query;
-
-}
-
+// export default function (req, res, next) {
+//   res.locals.url = req.url;
+//   res.locals.errors = null;
+//   res.locals.query = req.query;
+// }
